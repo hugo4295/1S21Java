3f10f36e6691ab550ee1ee2ddf90b5ff7bcfd8fd
victor hugo de la o martinez <hugo_4295@hotmail.com>
2023-10-10 14:18:53 -0600

    concentrado
    practicas p1

---
+++ p1/P12_1S2.java
@@ -0,0 +1,27 @@
+class P12_1S21{
+	public static void main(String[] args) {
+
+		int a;
+
+		for(a=0;a<=10;a++){
+			System.out.println("el valor para a es " +a);
+		}
+		System.out.println();
+		System.out.println();
+		a=10;
+		for (;a<20;a+=2){ //a=a+2
+			System.out.println("Valor de a nuevo " + a);
+		}
+		System.out.println();
+		System.out.println();
+		for (a=20;a>0;a--) { // a=a-1
+			System.out.println("valor en decremento de a " + a);
+		}
+		a=1;
+		for (; ; ) {
+			System.out.println("Valro de a " + a);
+			if (a>10) break;
+			a++; 
+		}
+	}
+}
